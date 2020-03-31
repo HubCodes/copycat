3d331ab88fabc979e904937b0d5bc86f031a5299
Taemin Cha <krlrhkstk12@gmail.com>
2020-03-31 18:36:49 +0900

    Add implementation scenarios from stdin or from args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs;
+use std::io::{stdin, stdout, Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let args: &[String] = &args[1..];
+    match args {
+        [] => from_stdin(),
+        _ => from_args(args),
+    }
+}
+
+fn from_args(args: &[String]) {
+    for arg in args {
+        let contents = fs::read(&arg)
+            .expect(format!("An error occurred while reading filename {}", &arg).as_str());
+        stdout()
+            .write(&contents)
+            .expect(format!("An error occurred while writing filename {}", &arg).as_str());
+    }
+}
+
+fn from_stdin() {
+    let mut contents: Vec<u8> = Vec::new();
+    stdin().read(&mut contents).expect("Cannot read from stdin");
+    stdout().write(&contents).expect("Cannot write to stdout");
 }
